22a29088b59a1f335eaa093016b3714fd184c3ab
Abdulrazak Osman <72877276+iamabdulrazak@users.noreply.github.com>
2020-11-01 23:27:42 +0300

    fixing bugs!

--- script.py
+++ script.py
@@ -57,3 +57,42 @@ df.loc[X_val, 'data_type'] = 'val'
 
 df.groupby(['category', 'label', 'data_type']).count()
 
+# Loading Tokenizer and Encoding our Data
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased',
+                                          do_lower_case=True)
+# after we load the tokenizer we gonna encode the data!
+# we will; make train and validation encoded!
+encoded_data_train = tokenizer.batch_encode_plus(
+    df[df.data_type=='train'].text.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+encoded_data_val = tokenizer.batch_encode_plus(
+    df[df.data_type=='val'].text.values,
+    add_special_tokens=True,
+    return_attention_mask=True,
+    pad_to_max_length=True,
+    max_length=256,
+    return_tensors='pt'
+)
+
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
+labels_train = torch.tensor(df[df.data_type=='train'].label.values)
+
+input_ids_val = encoded_data_val['input_ids']
+attention_masks_val = encoded_data_val['attention_mask']
+labels_val = torch.tensor(df[df.data_type=='val'].label.values)
+
+# saving encoded data into two dataset (dataset_train, dataset_val(test))!
+# then let us see the shape of it both!
+dataset_train = TensorDataset(input_ids_train, attention_masks_train, labels_train)
+dataset_val = TensorDataset(input_ids_val, attention_masks_val, labels_val)
+
+print(f'Train Dataset: {len(dataset_train)}')
+print(f'Test Dataset: {len(dataset_val)}')
