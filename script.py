07bd6f9adf1b2879ca6fd5a1482a57b892e8db2f
Abdulrazak Osman <72877276+iamabdulrazak@users.noreply.github.com>
2020-11-01 23:56:14 +0300

    fixing bugs!

--- script.py
+++ script.py
@@ -145,3 +145,95 @@ def accuracy_per_class(preds, labels):
         y_true = labels_flat[labels_flat==label]
         print(f'Class: {label_dict_inverse[label]}')
         print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
+
+# Creating Training Loop and Evaluation
+# we used cuda aka GPU on google colab to make the prediction fast!
+seed_val = 17
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+# here we check if the device is gpu!
+# if not use cpu!
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+print(f'Device(cpu/gpu): {device}')
+
+# Evaluation
+def evaluate(dataloader_val):
+
+    model.eval()
+    loss_val_total = 0
+    predictions, true_vals = [], []
+
+    for batch in dataloader_val:
+        batch = tuple(b.to(device) for b in batch)
+
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        loss = outputs[0]
+        logits = outputs[1]
+        loss_val_total += loss.item()
+
+        logits = logits.detach().cpu().numpy()
+        label_ids = inputs['labels'].cpu().numpy()
+        predictions.append(logits)
+        true_vals.append(label_ids)
+
+    loss_val_avg = loss_val_total/len(dataloader_val)
+
+    predictions = np.concatenate(predictions, axis=0)
+    true_vals = np.concatenate(true_vals, axis=0)
+
+    return loss_val_avg, predictions, true_vals
+
+# Predicting
+
+for epoch in tqdm(range(1, epochs+1)):
+
+    model.train()
+    loss_train_total = 0
+
+    progress_bar = tqdm(dataloader_train, desc='Epoch {:1d}'.format(epoch), leave=False, disable=False)
+    for batch in progress_bar:
+
+        model.zero_grad()
+
+        batch = tuple(b.to(device) for b in batch)
+
+        inputs = {'input_ids':      batch[0],
+                  'attention_mask': batch[1],
+                  'labels':         batch[2],
+                 }
+
+        outputs = model(**inputs)
+
+        loss = outputs[0]
+        loss_train_total += loss.item()
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+        optimizer.step()
+        scheduler.step()
+
+        progress_bar.set_postfix({'training_loss': '{:.3f}'.format(loss.item()/len(batch))})
+
+	torch.save(model.state_dict(), f'Models/finetuned_BERT_epoch_{epoch}.model')
+
+    tqdm.write(f'\nEpoch {epoch}')
+
+    loss_train_avg = loss_train_total/len(dataloader_train)
+    tqdm.write(f'Training loss: {loss_train_avg}')
+
+    val_loss, predictions, true_vals = evaluate(dataloader_validation)
+    val_f1 = f1_score_func(predictions, true_vals)
+    tqdm.write(f'Validation loss: {val_loss}')
+    tqdm.write(f'F1 Score (Weighted): {val_f1}')
