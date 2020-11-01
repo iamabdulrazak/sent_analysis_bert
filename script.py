e0ae5dfad419f9f797dc9566e8916eaea8c25bc9
Abdulrazak Osman <72877276+iamabdulrazak@users.noreply.github.com>
2020-11-01 23:41:55 +0300

    Defining Performance Metrics!

--- script.py
+++ script.py
@@ -127,3 +127,21 @@ scheduler = get_linear_schedule_with_warmup(optimizer,
                                             num_training_steps=len(dataloader_train)*epochs)
 
 
+# Defining Performance Metrics
+# we found that f1-score average (weighted or micro)
+# was hitting more accuracy and prediction rates!
+def f1_score_func(preds, labels):
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return f1_score(labels_flat, preds_flat, average='weighted')
+
+def accuracy_per_class(preds, labels):
+    label_dict_inverse = {v: k for k, v in label_dict.items()}
+    preds_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    for label in np.unique(labels_flat):
+        y_preds = preds_flat[labels_flat==label]
+        y_true = labels_flat[labels_flat==label]
+        print(f'Class: {label_dict_inverse[label]}')
+        print(f'Accuracy: {len(y_preds[y_preds==label])}/{len(y_true)}\n')
